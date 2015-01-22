7c8081a4c4c9902221e07a2d66b85cbd171eb7c2
Kara Leary <learykara@gmail.com>
2015-01-22 08:09:31 -0500

    accept more yes/no cases, require at least 1 taste

--- pirate_bartender.py
+++ pirate_bartender.py
@@ -1,3 +1,13 @@
+"""
+
+    pirate_bartender.py
+    ~~~~~~~~~~~~~~~~~~~
+    PIP-001 Assignment: generates a drink w/ a unique name
+    based on a user's tastes
+
+"""
+
+
 from random import choice
 
 
@@ -9,6 +19,7 @@ questions = {
     "fruity": "Are ye one for a fruity finish?"
 }
 
+
 ingredients = {
     "strong": ["glug of rum", "slug of whisky", "splash of gin"],
     "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
@@ -16,6 +27,7 @@ ingredients = {
     "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
     "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
 }
+
 
 drink_nouns = [
     'booty',
@@ -27,16 +39,18 @@ drink_nouns = [
     'treasure'
 ]
 
+
 def valid_response(resp):
     """Validates the response entered
     
     :param resp: the response entered in the console
     """
-    if resp.lower() in ('y', 'yes'):
+    if resp.lower()[0] == 'y':
         return True
-    elif resp.lower() in ('n', 'no'):
+    elif resp.lower()[0] == 'n':
         return False
     return None
+
 
 def get_customer_tastes():
     """Returns a ``dict`` indicating a customer's tastes"""
@@ -49,6 +63,7 @@ def get_customer_tastes():
         tastes[question] = response
     return tastes
 
+
 def construct_drink(tastes):
     """Returns a ``list`` representing a drink
     
@@ -59,6 +74,7 @@ def construct_drink(tastes):
             ingredients[item]) for item in tastes if tastes[item] is True]
     return drink
 
+
 def construct_drink_name(tastes):
     """Return the name of a drink, based on preferred tastes and a 
     random noun
@@ -66,7 +82,7 @@ def construct_drink_name(tastes):
     :param tastes: a ``dict`` indicateing the customer's tastes
     """
     noun = choice(drink_nouns)
-    adj = choice([k for k in tastes if tastes[k] is True])
+    adj = choice([k for k in tastes if tastes[k]])
     return adj + ' ' + noun
 
 
@@ -74,9 +90,14 @@ if __name__ == '__main__':
     have_another = True
     while have_another:
         tastes = get_customer_tastes()
-        drink = construct_drink(tastes)
+        if len([k for k in tastes if tastes[k]]) == 0:
+            print(
+                'You can\'t enjoy a drink if you don\'t like any of the'
+                ' ingredients! Try again.')
+            continue
+        ingredients = construct_drink(tastes)
         drink_name = construct_drink_name(tastes)
         print 'You get the {} (ingredients: {}). Enjoy!'.format(
-            drink_name, drink)
+            drink_name, ', '.join(ingredients))
         have_another = valid_response(raw_input('Would you like another?: '))
 
