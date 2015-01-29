14e18446b71881b5d54ac95446f4b8f1c7db824c
Kara Leary <learykara@gmail.com>
2015-01-29 08:44:11 -0500

    add max_bid and bid_on_item functions

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -29,6 +29,13 @@ class Item(Base):
     owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     bids = relationship('Bid', uselist=True, backref='bids_on_item')
 
+    def max_bid(self):
+        """Return the price of the highest bid on the item
+        If no bids have been placed, return 0
+        """
+        bids = session.query(Bid).filter_by(item_id = self.id)
+        return [0 if len(bids) == 0 else max([bid.price for bid in bids])]
+
 class User(Base):
     __tablename__ = 'users'
 
@@ -38,6 +45,25 @@ class User(Base):
 
     items_for_sale = relationship('Item', uselist=True, backref='owner')
     bids = relationship('Bid', uselist=True, backref='bid_owner')
+
+    def bid_on_item(self, item_id, price):
+        """Add an entry to the `bids` table representing a bid on the
+        Item with id = `item_id`
+
+        If the item doesn't exist, raise an error
+
+        Return the id of the bid
+        """
+        item = session.query(Item).get(item_id)
+        if item is None:
+            # return error? none?
+            pass
+        if price <= item.max_bid():
+            # return error?
+            pass
+        bid_made = Bid(price=price, item_id=item_id, user_id=self.id)
+        return bid_made.id
+
 
 class Bid(Base):
     __tablename__ = 'bids'
