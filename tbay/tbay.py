47c3ab8ff00e9530c6162c78db70688b25a0afcc
Kara Leary <learykara@gmail.com>
2015-01-29 07:49:40 -0500

    Attempt at relationships between Items and Users

--- tbay/tbay.py
+++ tbay/tbay.py
@@ -2,8 +2,9 @@
 from datetime import datetime
 
 from sqlalchemy import (
-    create_engine, Column, Integer, String, DateTime, Float)
-from sqlalchemy.orm import sessionmaker
+    create_engine, Column, Integer, String, DateTime, Float,
+    ForeignKey, Table)
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -11,6 +12,11 @@ engine = create_engine('postgresql://karaleary:karaleary@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
+bid_table = Table(
+    'tbay_bids', Base.metadata,
+    Column('item_id', Integer, ForeignKey('items.id')),
+    Column('user_id', Integer, ForeignKey('users.id')))
 
 class Item(Base):
     __tablename__ = "items"
@@ -20,12 +26,17 @@ class Item(Base):
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
 
+    owner_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    bids = relationship("User", secondary="tbay_bids", backref="items_bid_on")
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
+
+    items_for_sale = relationship("Item", uselist=True, backref="owner_id")
 
 class Bid(Base):
     __tablename__ = "bids"
