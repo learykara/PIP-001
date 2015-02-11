5ea435051e296be4649a887479d7c3a0bd34cd67
Kara Leary <learykara@gmail.com>
2015-02-11 16:58:02 -0700

    blog

---
+++ blog/blog/views.py
@@ -0,0 +1,95 @@
+from flask import render_template, request, redirect, url_for
+import mistune
+
+from blog import app
+from database import session
+from models import Post
+
+
+@app.route('/')
+@app.route('/page/<int:page>')
+def posts(page=1, paginate_by=10):
+    # Zero-indexed page
+    page_index = page - 1
+    count = session.query(Post).count()
+
+    start = page_index * paginate_by
+    end = start + paginate_by
+
+    total_pages = (count - 1) / paginate_by + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    posts = session.query(Post)
+    posts = posts.order_by(Post.datetime.desc())
+    posts = posts[start:end]
+    return render_template(
+        'posts.html',
+        posts=posts,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
+    )
+
+
+@app.route('/post/add', methods=["GET"])
+def add_post_get():
+    return render_template('add_post.html')
+
+
+@app.route('/post/add', methods=["POST"])
+def add_post_post():
+    post = Post(
+        title=request.form['title'],
+        content=mistune.markdown(request.form['content'])
+    )
+    session.add(post)
+    session.commit()
+    return redirect(url_for('posts'))
+
+
+@app.route('/post/<post_id>')
+def view_post(post_id):
+    post = session.query(Post).filter_by(id=post_id).first()
+    if post is None:
+        return redirect(url_for('posts'))
+    return render_template(
+        'view_post.html',
+        post=post
+    )
+
+
+@app.route('/post/<post_id>/edit', methods=["GET"])
+def edit_post_get(post_id):
+    post = session.query(Post).filter_by(id=post_id).first()
+    if post is None:
+        return redirect(url_for('posts'))
+    return render_template(
+        'edit_post.html',
+        post=post
+    )
+
+
+@app.route('/post/<post_id>/edit', methods=["POST"])
+def edit_post_post(post_id):
+    title = request.form['title']
+    content = mistune.markdown(request.form['content'])
+    session.query(Post).filter_by(id=post_id).update({
+        'title': title, 'content': content})
+    session.commit()
+    return redirect(url_for('view_post', post_id=post_id))
+
+
+@app.route('/post/<post_id>/delete', methods=["GET"])
+def delete_post_get(post_id):
+    post = session.query(Post).filter_by(id=post_id).first()
+    return render_template('delete_post.html', post=post)
+
+
+@app.route('/post/<post_id>/delete', methods=["POST"])
+def delete_post_post(post_id):
+    post = session.query(Post).filter_by(id=post_id).first()
+    session.delete(post)
+    session.commit()
+    return redirect(url_for('posts'))
--- blog/manage.py
+++ blog/manage.py
@@ -3,6 +3,8 @@ import os
 from flask.ext.script import Manager
 
 from blog import app
+from blog.database import session
+from blog.models import Post
 
 
 manager = Manager(app)
@@ -14,6 +16,25 @@ def run():
     app.run(port=port)
 
 
+@manager.command
+def seed():
+    content = (
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do"
+        " eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut"
+        " enim ad minim veniam, quis nostrud exercitation ullamco laboris"
+        " nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor"
+        " in reprehenderit in voluptate velit esse cillum dolore eu fugiat"
+        " nulla pariatur. Excepteur sint occaecat cupidatat non proident,"
+        " sunt in culpa qui officia deserunt mollit anim id est laborum.""")
+
+    for i in range(25):
+        post = Post(
+            title="Test Post #{}".format(i),
+            content=content
+        )
+        session.add(post)
+    session.commit()
+
+
 if __name__ == '__main__':
     manager.run()
-
