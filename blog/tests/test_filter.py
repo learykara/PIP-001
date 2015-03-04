421409a3d4554972a1a586d78ad6d4eb7293830d
Kara Leary <learykara@gmail.com>
2015-03-03 23:25:55 -0500

    add initial unit tests

---
+++ blog/tests/test_filter.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+import datetime
+
+os.environ['CONFIG_PATH'] = 'blog.config.TestingConfig'
+
+import blog
+from blog.filters import dateformat
+
+
+class FilterTests(unittest.TestCase):
+
+    def test_date_format(self):
+        date = datetime.date(1999, 12, 31)
+        formatted = dateformat(date, '%y/%m/%d')
+        self.assertEqual(formatted, '99/12/31')
+
+    def test_date_format_none(self):
+        formatted = dateformat(None, '%y/%m/%d')
+        self.assertEqual(formatted, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
