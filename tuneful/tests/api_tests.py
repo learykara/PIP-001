9953e19344e13ab5ee15da9a8aef21e5ee6aecd6
Kara Leary <learykara@gmail.com>
2015-03-22 15:08:10 -0400

    add GET method for songs

--- tuneful/tests/api_tests.py
+++ tuneful/tests/api_tests.py
@@ -6,11 +6,11 @@ from urlparse import urlparse
 from StringIO import StringIO
 
 import sys; print sys.modules.keys()
-# Configure our app to use the testing databse
+
 os.environ["CONFIG_PATH"] = "tuneful.config.TestingConfig"
 
 from tuneful import app
-from tuneful import models
+from tuneful.models import File, Song
 from tuneful.utils import upload_path
 from tuneful.database import Base, engine, session
 
@@ -36,4 +36,21 @@ class TestAPI(unittest.TestCase):
         # Delete test upload folder
         shutil.rmtree(upload_path())
 
+    def seed_db(self):
+        """Seed the database with songs and files"""
+        for x in range(3):
+            file = File(filename='test_song_{}.mp3'.format(x + 1)).save()
+            Song(file_id=file.id).save()
 
+    def test_get_all_songs(self):
+        self.seed_db()
+
+        response = self.client.get('/api/songs')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, 'application/json')
+
+        data = json.loads(response.data)
+        self.assertEqual(len(data), 3)
+
+        song = data[0]
+        self.assertEqual(song.get('file').get('name'), 'test_song_1.mp3')
--- tuneful/tuneful/api.py
+++ tuneful/tuneful/api.py
@@ -5,10 +5,18 @@ from flask import request, Response, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from jsonschema import validate, ValidationError
 
-import models
 import decorators
+from models import Song, File
 from tuneful import app
 from database import session
 from utils import upload_path
 
 
+MIMETYPE = 'application/json'
+
+
+@app.route('/api/songs', methods=['GET'])
+def get_songs():
+    """Return a json object of all the songs in the db"""
+    songs = [song.to_dict() for song in session.query(Song).all()]
+    return Response(json.dumps(songs), 200, mimetype=MIMETYPE)
